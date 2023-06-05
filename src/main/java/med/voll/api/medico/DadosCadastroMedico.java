8b129cdcb0f4a892b47f36f998beb1f7006dc8d4
Diegocoderdab <diego.ab.eletricista@gmail.com>
2023-06-05 20:17:42 -0300

    As notations do BEANVALIDATION foram implementadas e estão com comentarios para futuras consultas

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,6 +1,24 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
+public record DadosCadastroMedico(
+
+        @NotBlank //nem nulo nem vazio
+        String nome,
+        @NotBlank
+        String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull //Somente valida se vem vazio o proprio Spring valida no ENUM
+        Especialidade especialidade,
+        @NotNull
+        @Valid //BEANVALIDATION valida-rá também esse DadosEnd que tb é um DTO
+        DadosEndereco endereco
+    ) {
 }
